eeaac8d5a1f57903b85ed34a4fdfe7a352a4198a
ManasNagaraj <knmanas6@gmail.com>
2021-05-16 18:06:29 +0530

    readme

--- setup.py
+++ setup.py
@@ -4,14 +4,12 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name='pyresparser',
-    version='1.0.6',
-    description='A simple resume parser used for extracting information from resumes',
-    long_description=open('README.rst').read(),
-    url='https://github.com/OmkarPathak/pyresparser',
-    author='Omkar Pathak',
-    author_email='omkarpathak27@gmail.com',
-    license='GPL-3.0',
+   
+   
+    url='https://github.com/ManasNagaraj/ResumeExtract.git',
+    author='Manas Nagaraj',
+    
+    
     include_package_data=True,
     classifiers=[
         'Intended Audience :: Developers',
@@ -54,7 +52,5 @@ setup(
         'wasabi>=0.2.2'
     ],
     zip_safe=False,
-    entry_points = {
-        'console_scripts': ['pyresparser=pyresparser.command_line:main'],
-    }
+    
 )
